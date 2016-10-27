2d1b062fac45d889ec7ee23ac21af3c4964fd9aa
Bill Gottsch <billgottsch@gmail.com>
2016-10-27 12:00:01 -0500

    repetition learning

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// for (var i=0; i < ; i++ ){
+//
+//
+//   if(i >2){
+//   console.log('watsup' + 1)
+// } else {
+//   console.log('DOGE' + 1)
+//   }
+//
+// }
+//
+//
+//
+// bitcoinPrice = 370;
+//
+// while(bitcoinPrice <400) {
+//
+//   console.log('hold at price: ' + bitcoinPrice);
+//
+//
+//     bitcoinPrice = bitcoinPrice + 10;
+// }
+//
+// console.log('SELL at price: ' + bitcoinPrice);
+
+// function reverse(s){
+//   return s.split("").reverse().join("");
+// }
+//
+// var jellyBeanFlavors = ['grape', 'cherry', 'watermelon'];
+// var newJellyBeanFlavors =[];
+// jellyBeanFlavors.forEach(function(flavor){
+//     newJellyBeanFlavors.push(reverse(flavor));
+//
+//
+// });
+//
+// console.log(newJellyBeanFlavors);
+
+
+
+// Insurance wants:
+// ['DOROTHY', 'SPARKY', 'SAM', 'HODOR', 'SIR EDWARD MCFLY']
